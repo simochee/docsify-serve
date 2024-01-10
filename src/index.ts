cb4b1f496aa5b209e0bea0ba9524a127df7c2f0e
simochee <lollipop@simochee.net>
2024-01-10 20:06:26 +0900

    feat: add serve command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,63 @@
 #!/usr/bin/env node
+import { program } from "commander";
+import type { IncomingMessage, NextFunction } from "connect";
+import liveServer from "live-server";
+import { readFile } from "node:fs/promises";
+import type { ServerResponse } from "node:http";
+import { join } from "node:path";
 
-console.log("cli ready");
+program
+  .name("docsify-serve")
+  .argument("[path]", "path to serve")
+  .helpOption("-h, --help", "display help for command")
+  .option("-o, --open", "automatically open a browser")
+  .option("--no-open", "suppress opening a browser")
+  .option("-p, --port <PORT>", "port to use", "3000")
+  .option("-H, --host <HOST>", "* host to use", "0.0.0.0")
+  .option("--ignore <PATH>", "* path to ignore")
+  .option("-q, --quiet", "* suppress logging")
+  .option("-V, --verbose", "* more logging")
+  .option(
+    "--wait <MILLISECOND>",
+    "* wait for all changes, before reloading",
+    "300",
+  )
+  .version("1.0.0", "-v, --version", "display version for command");
+
+program.parse();
+
+const { port, host, open, ignore, wait, quiet, verbose } = program.opts();
+const root = program.args[0] || "";
+
+const fallback = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: NextFunction,
+) => {
+  const url = req.url ?? "";
+
+  if (!/\.md$/i.test(url)) {
+    return next();
+  }
+
+  try {
+    const path = join(root, url);
+
+    res.write(await readFile(path, "utf-8"));
+  } catch {
+    res.writeHead(404);
+  }
+
+  res.end();
+};
+
+liveServer.start({
+  port: Number(port) || 3000,
+  host,
+  root,
+  open: open === true,
+  ignore,
+  wait: Number(wait) || 300,
+  logLevel: quiet ? 0 : verbose ? 2 : 1,
+  middleware: [fallback],
+});
